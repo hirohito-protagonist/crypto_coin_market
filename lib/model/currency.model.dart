cbe437366ab1c0fb5010c769183b364fb07013c8
hirohito-protagonist <hirohito.protagonist@protonmail.com>
2018-09-22 18:59:17 +0200

    chore: update packages and add more currencies

--- lib/model/currency.model.dart
+++ lib/model/currency.model.dart
@@ -2,7 +2,13 @@
 class Currency {
 
   static List<String> _codes = [
-    'USD', 'USDT', 'EUR', 'GBP', 'CNY'
+    'USD', 'USDT', 'EUR', 'GBP', 'CNY', 'JPY', 'RUB', 'AUD', 'CAD', 'PLN',
+    'SGD', 'CHF', 'HKD', 'INR', 'ZAR', 'LTC', 'KRW', 'NZD', 'BRL', 'SEK',
+    'BITUSD', 'UAH', 'ILS', 'THB', 'VND', 'ETH', 'BITCNY', 'RUR', 'DKK', 'MXN',
+    'CZK', 'CLP', 'TRY', 'NGN', 'IDR', 'MYR', 'PHP', 'ARS', 'IRR', 'MUR', 'DASH',
+    'UGX', 'BHD', 'XRP', 'COP', 'HRK', 'TZS', 'RON', 'CNH', 'PEN', 'BGN', 'PKR',
+    'NOK', 'HUF', 'GHS', 'DOGE', 'TUSD', 'DAI', 'QC', 'VEF', 'KHR', 'AED', 'CRC',
+    'KES', 'UZS', 'JOD', 'LKR', 'VES', 'BWP', 'ISK'
   ];
 
   static List<String> availableCurrencies() {
