46c5a1ecf2a560f251146ce7880f975e2471b37e
hirohito-protagonist <hirohito.protagonist@protonmail.com>
2018-12-12 18:38:23 +0100

    refactor: change to redux architecture
    Create initial flow to setup tools

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,12 @@ import 'package:crypto_coin_market/views/markets.view.dart';
 import 'package:crypto_coin_market/views/details.view.dart';
 import 'package:crypto_coin_market/model/details_view.model.dart';
 
-void main() => runApp(new MarketsPage());
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:redux/redux.dart';
+import 'package:crypto_coin_market/redux/reducers.dart';
+
+//void main() => runApp(CoinMarketApp());
+void main() => runApp(MarketsPage());
 
 class MarketsPage extends StatelessWidget {
   @override
@@ -14,16 +19,61 @@ class MarketsPage extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: new MarketsView(
+          title: 'Crypto Coin Market',
+          onSelect: (context, DetailsViewModel data) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => DetailsView(
+                title: 'Detail information',
+                data: data,
+              )),
+            );
+          }
+      ),
+    );
+  }
+}
+
+
+class CoinMarketApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    final Store<AppState> store = Store<AppState>(
+      appStateReducer,
+      initialState: AppState.initialState(),
+    );
+
+    return StoreProvider<AppState>(
+      store: store,
+      child: MaterialApp(
         title: 'Crypto Coin Market',
-        onSelect: (context, DetailsViewModel data) {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => DetailsView(
-              title: 'Detail information',
-              data: data,
-            )),
+        theme: new ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: MarketsViewPage(),
+      )
+    );
+  }
+}
+
+
+class MarketsViewPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Markets'),
+        actions: <Widget>[
+        ],
+      ),
+      body: StoreConnector<AppState, String>(
+        converter: (Store<AppState> store) => store.state.activeCurrency,
+        builder: (BuildContext context, String currency) {
+          return Container(
+            child: Text('${currency}'),
           );
-        }
+        },
       ),
     );
   }
