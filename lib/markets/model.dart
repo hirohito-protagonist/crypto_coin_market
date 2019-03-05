d5091a446d8e5f359b76adde4fafd493e285de14
hirohito-protagonist <hirohito.protagonist@protonmail.com>
2019-03-05 19:51:28 +0100

    chore: clean up model

--- lib/markets/model.dart
+++ lib/markets/model.dart
@@ -1,37 +1,6 @@
 import 'package:flutter/foundation.dart';
 
-import 'package:crypto_coin_market/data_source/data_source.dart';
-
-class MarketsModel {
-
-  final List<TotalVolume> volume;
-  final MultipleSymbols prices;
-
-  MarketsModel({
-    @required this.volume,
-    @required this.prices
-  });
-
-  _VolumeItemModel volumeItem(int index) {
-    final currency = volume[index]?.coinInfo?.name;
-    final displayPriceNode = prices.display.containsKey(currency) ? prices.display[currency] : null;
-    final rawPriceNode = prices.raw.containsKey(currency) ? prices.raw[currency] : null;
-    final price = displayPriceNode != null ? Map.of(displayPriceNode).values.toList()[0]['PRICE'] : '';
-    final priceChangeDisplay = displayPriceNode != null ? Map.of(displayPriceNode).values.toList()[0]['CHANGEPCT24HOUR'] : '';
-    final priceChange = rawPriceNode != null ? Map.of(rawPriceNode).values.toList()[0]['CHANGEPCT24HOUR'] : 0;
-
-    return _VolumeItemModel(
-      name: currency,
-      imageUrl: volume[index]?.coinInfo?.imageUrl,
-      fullName: volume[index]?.coinInfo?.fullName,
-      price: price,
-      priceChange: priceChange,
-      priceChangeDisplay: priceChangeDisplay,
-    );
-  }
-}
-
-class _VolumeItemModel {
+class VolumeItemModel {
 
   final String name;
   final String imageUrl;
@@ -40,7 +9,7 @@ class _VolumeItemModel {
   final String priceChangeDisplay;
   final num priceChange;
 
-  _VolumeItemModel({
+  VolumeItemModel({
     @required this.name,
     @required this.imageUrl,
     @required this.fullName,
--- lib/markets/selectors.dart
+++ lib/markets/selectors.dart
@@ -4,7 +4,7 @@ import 'package:crypto_coin_market/core/core.dart';
 import 'package:crypto_coin_market/data_source/data_source.dart';
 
 import './actions.dart';
-
+import './model.dart';
 
 class MarketsSelectors {
   final Store<AppState> store;
@@ -28,7 +28,7 @@ class MarketsSelectors {
   List<String> availableCurrencies() => this.store.state.marketsPageState.availableCurrencies;
   num activePage() => this.store.state.marketsPageState.page;
   ServiceDataState dataState() => this.dataSourceSelectors.volume().state;
-  List<_VolumeItemModel> data() {
+  List<VolumeItemModel> data() {
     final prices = this.dataSourceSelectors.prices().response;
     return this.dataSourceSelectors.volume().response.length == 0 ? List.unmodifiable([]) :
     this.dataSourceSelectors.volume().response.map((volume) =>
@@ -44,14 +44,14 @@ class MarketsSelectors {
   onRefresh() => this.store.dispatch(MarketsRefresh());
 }
 
-_VolumeItemModel _aggregateVolumeAndPrice(TotalVolume volume, MultipleSymbols prices) {
+VolumeItemModel _aggregateVolumeAndPrice(TotalVolume volume, MultipleSymbols prices) {
   final currency = volume?.coinInfo?.name;
   final displayPriceNode = prices.display.containsKey(currency) ? prices.display[currency] : null;
   final rawPriceNode = prices.raw.containsKey(currency) ? prices.raw[currency] : null;
   final price = displayPriceNode != null ? Map.of(displayPriceNode).values.toList()[0]['PRICE'] : '';
   final priceChangeDisplay = displayPriceNode != null ? Map.of(displayPriceNode).values.toList()[0]['CHANGEPCT24HOUR'] : '';
   final priceChange = rawPriceNode != null ? Map.of(rawPriceNode).values.toList()[0]['CHANGEPCT24HOUR'] : 0;
-  return _VolumeItemModel(
+  return VolumeItemModel(
     name: currency,
     imageUrl: volume?.coinInfo?.imageUrl,
     fullName: volume?.coinInfo?.fullName,
@@ -59,23 +59,4 @@ _VolumeItemModel _aggregateVolumeAndPrice(TotalVolume volume, MultipleSymbols pr
     priceChange: priceChange,
     priceChangeDisplay: priceChangeDisplay,
   );
-}
-
-class _VolumeItemModel {
-
-  final String name;
-  final String imageUrl;
-  final String fullName;
-  final String price;
-  final String priceChangeDisplay;
-  final num priceChange;
-
-  _VolumeItemModel({
-    @required this.name,
-    @required this.imageUrl,
-    @required this.fullName,
-    @required this.price,
-    @required this.priceChangeDisplay,
-    @required this.priceChange,
-  });
 }
