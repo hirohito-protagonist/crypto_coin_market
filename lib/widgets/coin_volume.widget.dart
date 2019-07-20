3e7b43d107e6d8b8ad6cc11b4ca72ef05fbdadab
hirohito-protagonist <hirohito.protagonist@protonmail.com>
2019-07-20 12:25:08 +0200

    feat: volume chart
    Slider for more information

--- lib/widgets/coin_volume.widget.dart
+++ lib/widgets/coin_volume.widget.dart
@@ -1,3 +1,6 @@
+import 'dart:math';
+
+import 'package:flutter/scheduler.dart';
 import 'package:flutter/material.dart';
 import 'package:charts_flutter/flutter.dart' as charts;
 import 'package:crypto_coin_market/data_source/data_source.dart';
@@ -19,8 +22,24 @@ class CoinVolumeWidget extends StatefulWidget {
 
 class CoinVolumeState extends State<CoinVolumeWidget> {
   List<HistogramDataModel> histData = [];
+  _SelectionChartSliderValue _selectionChartSliderValue = _SelectionChartSliderValue(
+    price: -1,
+    date: null,
+  );
 
   CoinVolumeState({this.histData});
+
+  @override
+  void initState() {
+    super.initState();
+    if (histData.length > 0) {
+      final index = (histData.length / 2).floor();
+      _selectionChartSliderValue = _SelectionChartSliderValue(
+        price: histData[index].close,
+        date: histData[index].time,
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -44,6 +63,7 @@ class CoinVolumeState extends State<CoinVolumeWidget> {
       charts.TimeSeriesChart(
         _createHistVolumeData(histData),
         animate: true,
+        defaultInteractions: false,
         domainAxis: charts.DateTimeAxisSpec(
           usingBarRenderer: true,
           showAxisLine: true,
@@ -58,19 +78,31 @@ class CoinVolumeState extends State<CoinVolumeWidget> {
           ),
         ),
         primaryMeasureAxis: charts.NumericAxisSpec(
-            tickProviderSpec: charts.BasicNumericTickProviderSpec(zeroBound: false),
-            showAxisLine: true,
-            renderSpec: charts.GridlineRendererSpec(
-              tickLengthPx: 2,
-              labelStyle: charts.TextStyleSpec(
-                color: charts.Color.fromHex(code: '#848eaf'),
-              ),
-              lineStyle: charts.LineStyleSpec(
-                color: charts.Color.fromHex(code: '#343c5c'),
-              ),
-            )
+          tickProviderSpec: charts.BasicNumericTickProviderSpec(zeroBound: false),
+          showAxisLine: true,
+          renderSpec: charts.GridlineRendererSpec(
+            tickLengthPx: 2,
+            labelStyle: charts.TextStyleSpec(
+              color: charts.Color.fromHex(code: '#848eaf'),
+            ),
+            lineStyle: charts.LineStyleSpec(
+              color: charts.Color.fromHex(code: '#343c5c'),
+            ),
+          )
         ),
         defaultRenderer: charts.BarRendererConfig<DateTime>(),
+        behaviors: [
+          charts.Slider(
+            initialDomainValue: _selectionChartSliderValue.date,
+            onChangeCallback: _onSliderChange,
+            style: charts.SliderStyle(
+              fillColor: charts.Color(a: 100, r: 122, g: 132, b: 166),
+              handleSize: Rectangle(0, 0, 40, 100),
+              strokeWidthPx: 0.0,
+            ),
+            snapToDatum: true,
+          )
+        ],
       ),
     );
   }
@@ -90,6 +122,25 @@ class CoinVolumeState extends State<CoinVolumeWidget> {
       )
     ];
   }
+
+  _onSliderChange(point, dynamic domain, charts.SliderListenerDragState dragState) {
+
+    void rebuild(_) {
+      if (dragState == charts.SliderListenerDragState.end) {
+        final histogramModel = histData.where((d) => d.time.isAtSameMomentAs(domain)).single;
+        if (histogramModel != null) {
+          setState(() {
+            _selectionChartSliderValue = _SelectionChartSliderValue(
+              date: domain,
+              price: histogramModel.close,
+            );
+          });
+        }
+      }
+    }
+
+    SchedulerBinding.instance.addPostFrameCallback(rebuild);
+  }
 }
 
 class LinearTime {
@@ -100,4 +151,11 @@ class LinearTime {
   final num volumeTo;
 
   LinearTime(this.close, this.time, this.high, this.low, this.volumeTo);
+}
+
+class _SelectionChartSliderValue {
+  final DateTime date;
+  final num price;
+
+  _SelectionChartSliderValue({this.date, this.price});
 }
