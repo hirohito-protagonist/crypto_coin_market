abe30ffdde0082cfc13a8d8e0f1dad5de2f4b04c
hirohito-protagonist <hirohito.protagonist@protonmail.com>
2019-08-13 21:49:18 +0200

    test: fix failing

--- test/widgets/coin_list_tile_test.dart
+++ test/widgets/coin_list_tile_test.dart
@@ -4,26 +4,22 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:crypto_coin_market/widgets/coin_list_tile.widget.dart';
 
 void main() {
-
-  testWidgets('Should render content with defualt data', (WidgetTester tester) async {
-
+  testWidgets('Should render content with defualt data',
+      (WidgetTester tester) async {
     await tester.pumpWidget(MaterialApp(
       home: Scaffold(
         body: ListView(
-          children: <Widget>[
-            CoinListTile()
-          ],
+          children: <Widget>[CoinListTile()],
         ),
       ),
     ));
 
-    print(tester.allWidgets.toList());
     expect(find.text(''), findsNWidgets(4));
     expect(find.text('%'), findsNothing);
   });
 
-  testWidgets('Should render content with provided data', (WidgetTester tester) async {
-
+  testWidgets('Should render content with provided data',
+      (WidgetTester tester) async {
     await tester.pumpWidget(MaterialApp(
       home: Scaffold(
         body: ListView(
@@ -32,7 +28,7 @@ void main() {
               name: 'BTC',
               fullName: 'Bitcoin',
               formattedPrice: '\$ 7000',
-              formattedPriceChange: '5.98',
+              formattedPriceChange: '5.98%',
               priceChange: 1.2,
             ),
           ],
@@ -40,15 +36,15 @@ void main() {
       ),
     ));
 
-
     expect(find.text('BTC'), findsOneWidget);
     expect(find.text('Bitcoin'), findsOneWidget);
     expect(find.text('\$ 7000'), findsOneWidget);
     expect(find.text('5.98%'), findsOneWidget);
   });
 
-  testWidgets('Should render price change as black when it was not change in the price', (WidgetTester tester) async {
-
+  testWidgets(
+      'Should render price change as black when it was not change in the price',
+      (WidgetTester tester) async {
     await tester.pumpWidget(MaterialApp(
       home: Scaffold(
         body: ListView(
@@ -61,14 +57,22 @@ void main() {
       ),
     ));
 
-    expect(find.byWidgetPredicate(
-      (Widget widget) => widget is Text && widget.style == TextStyle(color: const Color(0xFFa2aed0)),
-      description: 'Black text for unchanged price change',
-    ), findsOneWidget);
+    expect(
+        find.byWidgetPredicate(
+          (Widget widget) =>
+              widget is Text &&
+              widget.style ==
+                  TextStyle(
+                    color: const Color(0xFFa2aed0),
+                    fontWeight: FontWeight.bold,
+                  ),
+          description: 'Black text for unchanged price change',
+        ),
+        findsOneWidget);
   });
 
-  testWidgets('Should render price change as red when the price go down', (WidgetTester tester) async {
-
+  testWidgets('Should render price change as red when the price go down',
+      (WidgetTester tester) async {
     await tester.pumpWidget(MaterialApp(
       home: Scaffold(
         body: ListView(
@@ -81,14 +85,22 @@ void main() {
       ),
     ));
 
-    expect(find.byWidgetPredicate(
-          (Widget widget) => widget is Text && widget.style == TextStyle(color: const Color(0xFFff66b9)),
-      description: 'Red text for price change going down',
-    ), findsOneWidget);
+    expect(
+        find.byWidgetPredicate(
+          (Widget widget) =>
+              widget is Text &&
+              widget.style ==
+                  TextStyle(
+                    color: const Color(0xFFff66b9),
+                    fontWeight: FontWeight.bold,
+                  ),
+          description: 'Red text for price change going down',
+        ),
+        findsOneWidget);
   });
 
-  testWidgets('Should render price change as green when the price go up', (WidgetTester tester) async {
-
+  testWidgets('Should render price change as green when the price go up',
+      (WidgetTester tester) async {
     await tester.pumpWidget(MaterialApp(
       home: Scaffold(
         body: ListView(
@@ -101,9 +113,17 @@ void main() {
       ),
     ));
 
-    expect(find.byWidgetPredicate(
-          (Widget widget) => widget is Text && widget.style == TextStyle(color: const Color(0xFFa2ea76)),
-      description: 'Green text for price change going up',
-    ), findsOneWidget);
+    expect(
+        find.byWidgetPredicate(
+          (Widget widget) =>
+              widget is Text &&
+              widget.style ==
+                  TextStyle(
+                    color: const Color(0xFFa2ea76),
+                    fontWeight: FontWeight.bold,
+                  ),
+          description: 'Green text for price change going up',
+        ),
+        findsOneWidget);
   });
 }
--- test/widgets/price_change_test.dart
+++ test/widgets/price_change_test.dart
@@ -4,59 +4,82 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:crypto_coin_market/widgets/price_change.widget.dart';
 
 void main() {
-
-
-  testWidgets('Should render price change as black when it was not change in the price', (WidgetTester tester) async {
-
+  testWidgets(
+      'Should render price change as black when it was not change in the price',
+      (WidgetTester tester) async {
     await tester.pumpWidget(MaterialApp(
       home: Scaffold(
         body: PriceChange(
           change: 0,
-          price: '5.98',
+          price: '5.98%',
         ),
       ),
     ));
 
     expect(find.text('5.98%'), findsOneWidget);
-    expect(find.byWidgetPredicate(
-          (Widget widget) => widget is Text && widget.style == TextStyle(color: Colors.black),
-      description: 'Black text for unchanged price change',
-    ), findsOneWidget);
+    expect(
+        find.byWidgetPredicate(
+          (Widget widget) =>
+              widget is Text &&
+              widget.style ==
+                  TextStyle(
+                    color: const Color(0xFFa2aed0),
+                    fontWeight: FontWeight.bold,
+                  ),
+          description: 'Black text for unchanged price change',
+        ),
+        findsOneWidget);
   });
 
-  testWidgets('Should render price change as red when the price go down', (WidgetTester tester) async {
-
+  testWidgets('Should render price change as red when the price go down',
+      (WidgetTester tester) async {
     await tester.pumpWidget(MaterialApp(
       home: Scaffold(
         body: PriceChange(
           change: -2.5,
-          price: '5.98',
+          price: '5.98%',
         ),
       ),
     ));
 
     expect(find.text('5.98%'), findsOneWidget);
-    expect(find.byWidgetPredicate(
-          (Widget widget) => widget is Text && widget.style == TextStyle(color: Colors.red),
-      description: 'Red text for price change going down',
-    ), findsOneWidget);
+    expect(
+        find.byWidgetPredicate(
+          (Widget widget) =>
+              widget is Text &&
+              widget.style ==
+                  TextStyle(
+                    color: const Color(0xFFff66b9),
+                    fontWeight: FontWeight.bold,
+                  ),
+          description: 'Red text for price change going down',
+        ),
+        findsOneWidget);
   });
 
-  testWidgets('Should render price change as green when the price go up', (WidgetTester tester) async {
-
+  testWidgets('Should render price change as green when the price go up',
+      (WidgetTester tester) async {
     await tester.pumpWidget(MaterialApp(
       home: Scaffold(
         body: PriceChange(
           change: 3,
-          price: '5.98',
+          price: '5.98%',
         ),
       ),
     ));
 
     expect(find.text('5.98%'), findsOneWidget);
-    expect(find.byWidgetPredicate(
-          (Widget widget) => widget is Text && widget.style == TextStyle(color: Colors.green),
-      description: 'Green text for price change going up',
-    ), findsOneWidget);
+    expect(
+        find.byWidgetPredicate(
+          (Widget widget) =>
+              widget is Text &&
+              widget.style ==
+                  TextStyle(
+                    color: const Color(0xFFa2ea76),
+                    fontWeight: FontWeight.bold,
+                  ),
+          description: 'Green text for price change going up',
+        ),
+        findsOneWidget);
   });
 }
